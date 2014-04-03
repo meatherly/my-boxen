d73e1dd7730966affc828c628d56c7a148008931
Michael Eatherly <meatherly@gmail.com>
2014-04-03 17:38:45 -0500

    adding mysql

---
+++ modules/people/meatherly.pp
@@ -0,0 +1,18 @@
+class people::meatherly {
+  include emacs   # requires emacs module in Puppetfile
+  include sparrow # requires sparrow module in Puppetfile
+  include mysql
+
+  $home     = "/Users/${::boxen_user}"
+  # $my       = "${home}/my"
+  # $dotfiles = "${my}/dotfiles"
+  
+  # file { $my:
+  #   ensure  => directory
+  # }
+
+  # repository { $dotfiles:
+  #   source  => 'jbarnette/dotfiles',
+  #   require => File[$my]
+  # }
+}
